60e3a27fdcb92c4a3e18089696ab74730c8989b1
Mahmoud Najmeh <mamocool3@gmail.com>
2026-03-18 09:24:45 +0100

    Dagster ETL Project

--- dagster_etl/schedules/etl_schedules.py
+++ dagster_etl/schedules/etl_schedules.py
@@ -3,7 +3,7 @@ from dagster import schedule
 @schedule(
     cron_schedule="0 9 * * *", 
     job_name="full_etl_job",
-    execution_timezone="US/Central"
+    execution_timezone="Europe/Berlin"
 )
 def daily_etl_schedule(context):
     """Schedule to run ETL daily"""
